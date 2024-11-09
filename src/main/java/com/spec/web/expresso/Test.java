2fe2988e85a73d9186f1bd86b9c3e44a8356b337
Neeraj Surjaye <neerajsurjaye@gmail.com>
2024-11-10 00:49:15 +0530

    Added Sample routes

--- src/main/java/com/spec/web/expresso/Test.java
+++ src/main/java/com/spec/web/expresso/Test.java
@@ -6,8 +6,18 @@ public class Test {
     public static void main(String[] args) {
         Expresso app = Expresso.init();
 
+        app.get("/about", (req, res) -> {
+            try {
+                res.setContentType("application/json");
+                res.getWriter().println("{\"inside\" : \"about\" , \"dataType\" : \"json\"}");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
         app.get("/home", (req, res) -> {
             try {
+                res.setContentType("text/html");
                 res.getWriter().println("in home path :)");
             } catch (IOException e) {
                 e.printStackTrace();
