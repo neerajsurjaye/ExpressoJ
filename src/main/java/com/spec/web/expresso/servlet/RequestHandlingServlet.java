c136b739f3ec9fab8e404f36d220a9b3978d9476
Neeraj Surjaye <neerajsurjaye@gmail.com>
2024-11-18 19:40:20 +0530

    feat/Added support for custom Http Request and response

--- src/main/java/com/spec/web/expresso/servlet/RequestHandlingServlet.java
+++ src/main/java/com/spec/web/expresso/servlet/RequestHandlingServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import com.spec.web.expresso.Expresso;
 import com.spec.web.expresso.callback.RequestCallback;
+import com.spec.web.expresso.message.HttpRequest;
+import com.spec.web.expresso.message.HttpResponse;
 import com.spec.web.expresso.router.ExpressoRouter;
 
 import jakarta.servlet.ServletException;
@@ -61,7 +63,10 @@ public class RequestHandlingServlet extends HttpServlet {
             return;
         }
 
-        callback.execute(req, resp);
+        HttpRequest request = new HttpRequest(req);
+        HttpResponse response = new HttpResponse(resp);
+
+        callback.execute(request, response);
 
     }
 
